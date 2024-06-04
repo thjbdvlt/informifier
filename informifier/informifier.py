58aef7522985bac39eb4dbc30fd2be115ba3c703
thjbbvlt <thibault2@ik.me>
2024-06-04 10:48:05 +0200

    corr

--- informifier/informifier.py
+++ informifier/informifier.py
@@ -7,44 +7,42 @@ class Informitif:
     """cherche l'infinitif d'un verbe du premier groupe."""
 
     terminaisons_premier_groupe = set(
-        [
-            # infinitif
-            "er",
-            # 1) indicatif
-            # 1.1) présent
-            ["e", "es", "e", "ons", "ez", "ent"]
-            # 1.2) imparfait
-            + ["ais", "ais", "ait", "ions", "iez", "aient"]
-            # 1.3) futur simple
-            + ["erai", "eras", "era", "erons", "erez", "eront"]
-            # 1.4) passé simple
-            + ["ai", "as", "a", "âmes", "âtes", "èrent"]
-            # 1.5) passé composé
-            + ["é", "é", "é", "é", "é", "é", "é"]
-            # 2) subjonctif
-            # 2.1) présent
-            + ["e", "es", "e", "ions", "iez", "ent"]
-            # 2.2) imparfait
-            + ["asse", "asses", "ât", "assions", "assiez", "assent"]
-            # 3) conditionnel
-            # 3.1) présent
-            + [
-                "erais",
-                "erais",
-                "erait",
-                "erions",
-                "eriez",
-                "eraient",
-            ]
-            # 4) impératif
-            # 4.1) présent
-            + ["e", "ons", "ez"]
-            # 5) participe
-            # 5.1) présent
-            + ["ant"]
-            # 5.2) passé
-            + ["é", "és", "ée", "ées"],
+        # infinitif
+        ["er"]
+        # 1) indicatif
+        # 1.1) présent
+        + ["e", "es", "e", "ons", "ez", "ent"]
+        # 1.2) imparfait
+        + ["ais", "ais", "ait", "ions", "iez", "aient"]
+        # 1.3) futur simple
+        + ["erai", "eras", "era", "erons", "erez", "eront"]
+        # 1.4) passé simple
+        + ["ai", "as", "a", "âmes", "âtes", "èrent"]
+        # 1.5) passé composé
+        + ["é", "é", "é", "é", "é", "é", "é"]
+        # 2) subjonctif
+        # 2.1) présent
+        + ["e", "es", "e", "ions", "iez", "ent"]
+        # 2.2) imparfait
+        + ["asse", "asses", "ât", "assions", "assiez", "assent"]
+        # 3) conditionnel
+        # 3.1) présent
+        + [
+            "erais",
+            "erais",
+            "erait",
+            "erions",
+            "eriez",
+            "eraient",
         ]
+        # 4) impératif
+        # 4.1) présent
+        + ["e", "ons", "ez"]
+        # 5) participe
+        # 5.1) présent
+        + ["ant"]
+        # 5.2) passé
+        + ["é", "és", "ée", "ées"]
     )
 
     def __init__(self):
